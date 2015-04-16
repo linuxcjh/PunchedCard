ea33381a63bc826a3b4d9b89a50fb9751a7deea2
Chenjianhui <e.cjh.cn@gmail.com>
2015-04-16 17:01:33 +0800

    Initial commit

---
+++ app/src/main/java/nuoman/com/punchedcard/MainActivity.java
@@ -0,0 +1,40 @@
+package nuoman.com.punchedcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_HOME:
+                return true;
+            case KeyEvent.KEYCODE_BACK:
+                return true;
+            case KeyEvent.KEYCODE_CALL:
+                return true;
+            case KeyEvent.KEYCODE_SYM:
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                return true;
+            case KeyEvent.KEYCODE_STAR:
+                return true;
+            default:
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
